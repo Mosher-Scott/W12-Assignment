8ca7ff303186e41c86c2afcfebbc1ea27d97beeb
Mosher-Scott <jorgemonkey81@byui.edu>
2020-03-22 16:13:01 -0600

    Added new page

---
+++ Notes/Notes/Notes/NoteEntryPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Notes.Models;
+
+namespace Notes
+{
+    public partial class NoteEntryPage : ContentPage
+    {
+        public NoteEntryPage()
+        {
+            InitializeComponent();
+        }
+
+        // Action taken when the user clicks the "Save" button
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            // Stores a note instance
+            var note = (Note)BindingContext;
+
+            if (string.IsNullOrWhiteSpace(note.Filename))
+            {
+                // Save
+                var filename = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.txt");
+                File.WriteAllText(filename, note.Text);
+            }
+            else
+            {
+                // Update
+                File.WriteAllText(note.Filename, note.Text);
+            }
+
+            await Navigation.PopAsync();
+        }
+
+        // Deletes the user specified note
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+
+            if (File.Exists(note.Filename))
+            {
+                File.Delete(note.Filename);
+            }
+
+            await Navigation.PopAsync();
+        }
+    }
+}
